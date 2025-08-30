58c43e359449880fe6a0acef5d19c7a951f5fef4
NathanASC25 <javanathanjchin@gmail.com>
2025-08-30 19:29:20 -0400

    first commit

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+public class Welcome{
+    public static void main(String[] args){
+        String address = "55 Avenue O West 9th Street";
+        String phoneNum = "347-728-1338";
+        System.out.println("\nWelcome to Java programming environment");
+        System.out.println("\n           by Nathan Chin");
+        System.out.println("\nAddress: " + address);
+        System.out.println("\nPhone Number: " + phoneNum);
+    }
+}
