fbf4ad58eebb3742676d93bcd0b491b9acebc82a
NathanASC25 <javanathanjchin@gmail.com>
2025-08-30 22:54:08 -0400

    second commit

---
+++ Chapter2_3_8.java
@@ -0,0 +1,10 @@
+public class Chapter2_3_8{
+    public static void main(String[] args) {
+        int x, y, z;
+
+        x = 2 + 3 / (4 - 1);
+        y = 10 * 3 * 3;
+        z = (1 + 4) / 5 * 2;
+        System.out.printf("\n%d %d %d\n", x, y, z);
+    }
+}
